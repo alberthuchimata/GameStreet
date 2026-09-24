17a99103cb4f5d90c4a38263c403ac957084b8cf
alberthuchimata <alberth.costa@me.com>
2026-09-24 16:33:15 -0400

    Delete ABRIR-JOGO.bat

--- ABRIR-JOGO.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-title Street Gay Fighter - Servidor Local
-start "" /b node server.cjs
-timeout /t 1 /nobreak >nul
-start "" http://127.0.0.1:8787
-echo Deixe esta janela aberta enquanto estiver jogando.
-pause
